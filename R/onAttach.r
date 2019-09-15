7f6bd1bdbccdfedcd03dbd50b6ba75ddbdc69b28
matthieu.lesnoff@gmail.com <matthieu.lesnoff@gmail.com>
2019-09-15 08:59:29 +0200

    1.0-3

--- R/onAttach.r
+++ R/onAttach.r
@@ -1,5 +1,6 @@
 .onAttach <- function(libname, pkgname){
     packageStartupMessage(
-      "Package rnirs version : 1.0.2 \n\n"
+      "Package rnirs version : 1.0.3 \n\n"
+      "\nYou may check at https://github.com/mlesnoff/rnirs for more recent versions.\n\n" 
       )
 }
