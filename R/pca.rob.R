48842ebacd29163be431d2a797937150e5052ea5
matthieu.lesnoff@gmail.com <matthieu.lesnoff@gmail.com>
2020-05-18 09:42:23 +0200

    1.6-12

--- R/pca.rob.R
+++ R/pca.rob.R
@@ -1,9 +1,9 @@
-pca.rob <- function(X, ncomp, nsim = 1000, alpha = .75) {
+pca.rob <- function(X, ncomp, nsim = 3000, alpha = .75) {
   
   d <- stahel(X, scale = TRUE, nsim = nsim)
   cutoff <- quantile(d, alpha)
   w1 <- ifelse(d < cutoff, 1, 0)
-
+  
   fm <- pca.svd(X, ncomp = ncomp, weights = w1)
   
   z <- odis(fm, X)
